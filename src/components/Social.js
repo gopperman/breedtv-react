82e829efdc1b563822a7c70a415a21008f641779
Greg Opperman <gopperman@gmail.com>
2017-07-19 09:33:43 -0400

    Social component css

--- src/components/Social.js
+++ src/components/Social.js
@@ -8,8 +8,9 @@ class Social extends Component {
       'overflow': 'hidden',
       'height': '35px',
     }
+    
     return (
-		<div id="social">
+		<div className="social">
 			<p><img src={logoSmall} alt="BreedTV" /></p>
 			<iframe className="social__like" title="like us on Facebook" src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fbreedtv&amp;width&amp;layout=button&amp;action=like&amp;show_faces=false&amp;share=false&amp;height=35" scrolling="no" frameborder="0" style={socialStyle} allowTransparency="true"></iframe>
 			<iframe className="social__follow" title="follow us on Twitter" allowtransparency="true" frameborder="0" scrolling="no"  src="//platform.twitter.com/widgets/follow_button.html?screen_name=breedtv&show_count=false&show_screen_name=false" style={socialStyle}></iframe>
