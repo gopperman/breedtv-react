b408000ee76834c861755721c4cff14ce99841f6
Greg Opperman <gopperman@gmail.com>
2017-07-19 09:47:08 -0400

    Colors an typography

--- src/components/Social.js
+++ src/components/Social.js
@@ -8,12 +8,12 @@ class Social extends Component {
       'overflow': 'hidden',
       'height': '35px',
     }
-    
+
     return (
 		<div className="social">
 			<p><img src={logoSmall} alt="BreedTV" /></p>
-			<iframe className="social__like" title="like us on Facebook" src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fbreedtv&amp;width&amp;layout=button&amp;action=like&amp;show_faces=false&amp;share=false&amp;height=35" scrolling="no" frameborder="0" style={socialStyle} allowTransparency="true"></iframe>
-			<iframe className="social__follow" title="follow us on Twitter" allowtransparency="true" frameborder="0" scrolling="no"  src="//platform.twitter.com/widgets/follow_button.html?screen_name=breedtv&show_count=false&show_screen_name=false" style={socialStyle}></iframe>
+			<iframe className="social__like" title="like us on Facebook" src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fbreedtv&amp;width&amp;layout=button&amp;action=like&amp;show_faces=false&amp;share=false&amp;height=35" scrolling="no" frameBorder="0" style={socialStyle} allowTransparency="true"></iframe>
+			<iframe className="social__follow" title="follow us on Twitter" allowTransparency="true" frameBorder="0" scrolling="no"  src="//platform.twitter.com/widgets/follow_button.html?screen_name=breedtv&show_count=false&show_screen_name=false" style={socialStyle}></iframe>
 		</div>
     );
   }
